8b6f48d5430e6bf2cfe5f475e81d8bfd6646c4a2
Matheusinh02 <161158314+Matheusinh02@users.noreply.github.com>
2024-05-26 18:31:53 -0300

    Update Restaurante.java

--- codigo/restaurante/src/main/java/com/calabreso/restaurante/entity/Restaurante.java
+++ codigo/restaurante/src/main/java/com/calabreso/restaurante/entity/Restaurante.java
@@ -1,139 +1,154 @@
 package com.calabreso.restaurante.entity;
 
-
 public class Restaurante {
 
-	private static int MAX_FILA = 1000;
-	private static int MAX_MESAS = 10;
-	private static int MAX_CLIENTES = 1000;
-	private Mesa[] mesas;
-	private Cliente[] clientes;
-	private Requisicao[] atendidas;
-	private Requisicao[] espera;
-	private int quantClientes;
-	private int quantMesas;
-	private int requisicoesAtendidas;
-	private int requisicoesEmEspera;
+    private static int MAX_FILA = 1000;
+    private static int MAX_MESAS = 10;
+    private static int MAX_CLIENTES = 1000;
+    private Mesa[] mesas;
+    private Cliente[] clientes;
+    private Requisicao[] atendidas;
+    private Requisicao[] espera;
+    private int quantClientes;
+    private int quantMesas;
+    private int requisicoesAtendidas;
+    private int requisicoesEmEspera;
+    private Cardapio cardapio; // Adicionado o cardápio
 
-	public Restaurante() {
-		mesas = new Mesa[MAX_MESAS];
-		clientes = new Cliente[MAX_CLIENTES];
-		atendidas = new Requisicao[MAX_FILA];
-		espera = new Requisicao[MAX_FILA];
-		quantMesas = quantClientes = requisicoesAtendidas = requisicoesEmEspera = 0;
-		criarMesas();
-	}
+    public Restaurante() {
+        mesas = new Mesa[MAX_MESAS];
+        clientes = new Cliente[MAX_CLIENTES];
+        atendidas = new Requisicao[MAX_FILA];
+        espera = new Requisicao[MAX_FILA];
+        quantMesas = quantClientes = requisicoesAtendidas = requisicoesEmEspera = 0;
+        cardapio = new Cardapio(); // Inicialização do cardápio
+        criarMesas();
+    }
 
-	private void criarMesas() {
+    private void criarMesas() {
+        for (int i = 0; i < 4; i++) {
+            mesas[quantMesas] = new Mesa(4);
+            quantMesas++;
+        }
+        for (int i = 0; i < 4; i++) {
+            mesas[quantMesas] = new Mesa(6);
+            quantMesas++;
+        }
+        for (int i = 0; i < 2; i++) {
+            mesas[quantMesas] = new Mesa(8);
+            quantMesas++;
+        }
+    }
 
-		for (int i = 0; i < 4; i++) {
-			mesas[quantMesas] = new Mesa(4);
-			quantMesas++;
-		}
-		for (int i = 0; i < 4; i++) {
-			mesas[quantMesas] = new Mesa(6);
-			quantMesas++;
-		}
-		for (int i = 0; i < 2; i++) {
-			mesas[quantMesas] = new Mesa(8);
-			quantMesas++;
-		}
-	}
+    public void addCliente(Cliente novo) {
+        clientes[quantClientes] = novo;
+        quantClientes++;
+    }
 
-	public void addCliente(Cliente novo) {
-		clientes[quantClientes] = novo;
-		quantClientes++;
-	}
+    public Cliente localizarCliente(int idCli) {
+        boolean achou = false;
+        Cliente cliente = null;
+        for (int i = 0; i < quantClientes && !achou; i++) {
+            if (clientes[i].hashCode() == idCli) {
+                achou = true;
+                cliente = clientes[i];
+            }
+        }
+        return cliente;
+    }
 
-	public Cliente localizarCliente(int idCli) {
-		boolean achou = false;
-		Cliente cliente = null;
-		for (int i = 0; i < quantClientes && !achou; i++) {
-			if (clientes[i].hashCode() == idCli) {
-				achou = true;
-				cliente = clientes[i];
-			}
-		}
-		return cliente;
-	}
+    private Mesa localizarMesaDisponivel(int quantPessoas) {
+        Mesa liberada = null;
+        for (int i = 0; i < quantMesas && liberada == null; i++) {
+            if (mesas[i].estahLiberada(quantPessoas))
+                liberada = mesas[i];
+        }
+        return liberada;
+    }
 
-	private Mesa localizarMesaDisponivel(int quantPessoas) {
-		Mesa liberada = null;
-		for (int i = 0; i < quantMesas && liberada == null; i++) {
-			if (mesas[i].estahLiberada(quantPessoas))
-				liberada = mesas[i];
-		}
-		return liberada;
-	}
+    public Requisicao encerrarAtendimento(int idMesa) {
+        Requisicao encerrada = null;
+        for (int i = requisicoesAtendidas - 1; i >= 0 && encerrada == null; i--) {
+            if (!atendidas[i].estahEncerrada() && atendidas[i].ehDaMesa(idMesa)) {
+                atendidas[i].encerrar();
+                encerrada = atendidas[i];
+            }
+        }
+        return encerrada;
+    }
 
-	public Requisicao encerrarAtendimento(int idMesa) {
-		Requisicao encerrada = null;
-		for (int i = requisicoesAtendidas - 1; i >= 0 && encerrada == null; i--) {
-			if (!atendidas[i].estahEncerrada() && atendidas[i].ehDaMesa(idMesa)) {
-				atendidas[i].encerrar();
-				encerrada = atendidas[i];
-			}
-		}
-		return encerrada;
-	}
+    public Requisicao processarFila() {
+        Requisicao atendida = null;
+        for (int i = 0; i < requisicoesEmEspera && atendida == null; i++) {
+            Requisicao requisicao = espera[i];
+            Mesa mesaLivre = localizarMesaDisponivel(requisicao.quantPessoas());
+            if (mesaLivre != null) {
+                atenderRequisicao(requisicao, mesaLivre);
+                retirarDaFila(i);
+                atendida = requisicao;
+            }
+        }
+        return atendida;
+    }
 
-	public Requisicao processarFila() {
-		Requisicao atendida = null;
-		for (int i = 0; i < requisicoesEmEspera && atendida == null; i++) {
-			Requisicao requisicao = espera[i];
-			Mesa mesaLivre = localizarMesaDisponivel(requisicao.quantPessoas());
-			if (mesaLivre != null) {
-				atenderRequisicao(requisicao, mesaLivre);
-				retirarDaFila(i);
-				atendida = requisicao;
-			}
-		}
-		return atendida;
-	}
+    private void retirarDaFila(int pos) {
+        for (int i = pos + 1; i < requisicoesEmEspera; i++) {
+            espera[pos] = espera[pos + 1];
+        }
+        requisicoesEmEspera--;
+        espera[requisicoesEmEspera] = null;
+    }
 
-	private void retirarDaFila(int pos) {
-		for (int i = pos + 1; i < requisicoesEmEspera; i++) {
-			espera[pos] = espera[pos + 1];
-		}
-		requisicoesEmEspera--;
-		espera[requisicoesEmEspera] = null;
-	}
+    public void registrarRequisicao(Requisicao novaRequisicao) {
+        espera[requisicoesEmEspera] = novaRequisicao;
+        requisicoesEmEspera++;
+    }
 
-	public void registrarRequisicao(Requisicao novaRequisicao) {
-		espera[requisicoesEmEspera] = novaRequisicao;
-		requisicoesEmEspera++;
-	}
+    private void atenderRequisicao(Requisicao requisicao, Mesa mesa) {
+        requisicao.alocarMesa(mesa);
+        atendidas[requisicoesAtendidas] = requisicao;
+        requisicoesAtendidas++;
+    }
 
-	private void atenderRequisicao(Requisicao requisicao, Mesa mesa) {
-		requisicao.alocarMesa(mesa);
-		atendidas[requisicoesAtendidas] = requisicao;
-		requisicoesAtendidas++;
-	}
+    public String statusMesas() {
+        StringBuilder livres = new StringBuilder("Mesas livres: \n");
+        StringBuilder ocupadas = new StringBuilder("Mesas em atendimento: \n");
+        for (int i = 0; i < quantMesas; i++) {
+            if (mesas[i].estahLiberada(1)) {
+                livres.append(mesas[i]).append("\n");
+            } else {
+                ocupadas.append(mesas[i]).append("\n");
+            }
+        }
+        return livres.toString() + ocupadas.toString();
+    }
 
-	public String statusMesas() {
-		StringBuilder livres = new StringBuilder("Mesas livres: \n");
-		StringBuilder ocupadas = new StringBuilder("Mesas em atendimento: \n");
-		for (int i = 0; i < quantMesas; i++) {
-			if (mesas[i].estahLiberada(1)) {
-				livres.append(mesas[i] + "\n");
-			}
-			else {
-				ocupadas.append(mesas[i] + "\n");
-			}
-		}
-		return livres.toString()+ocupadas.toString();
-	}
+    public String filaDeEspera() {
+        String resposta = "Fila vazia";
+        if (requisicoesEmEspera > 0) {
+            StringBuilder emEspera = new StringBuilder("Fila de espera: \n");
+            for (int i = 0; i < requisicoesEmEspera; i++) {
+                emEspera.append(espera[i]).append("\n");
+            }
+            resposta = emEspera.toString();
+        }
+        return resposta;
+    }
 
-	public String filaDeEspera() {
-		String resposta = "Fila vazia";
-		if (requisicoesEmEspera > 0) {
-			StringBuilder emEspera = new StringBuilder("Fila de espera: \n");
-			for (int i = 0; i < requisicoesEmEspera; i++) {
-					emEspera.append(espera[i]+"\n");
-			}
-			resposta =  emEspera.toString();
-		}
-		return resposta;
-	}
+    // Novos métodos para interagir com o Cardápio
+    public String exibirCardapioCompleto() {
+        return cardapio.exibirCardapio("1");
+    }
 
+    public String exibirBebidas() {
+        return cardapio.exibirCardapio("2");
+    }
+
+    public String exibirComidas() {
+        return cardapio.exibirCardapio("3");
+    }
+
+    public OpcaoCardapio getOpcaoCardapioById(int idOpcao) {
+        return cardapio.getOpcaoById(idOpcao);
+    }
 }
