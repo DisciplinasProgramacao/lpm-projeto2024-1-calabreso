e991a07e20eaede606be727a580397ef6432abfd
Matheusinh02 <161158314+Matheusinh02@users.noreply.github.com>
2024-05-27 09:33:16 -0300

    Update MesaController.java

--- codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/MesaController.java
+++ codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/MesaController.java
@@ -6,14 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.calabreso.restaurante.entity.Cliente;
 import com.calabreso.restaurante.entity.Mesa;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/mesas")
-public class MesaController {
+public class MesaController<MesaService> {
 
     private final MesaService mesaService;
 
@@ -23,26 +28,26 @@ public class MesaController {
 
     @PostMapping
     public Mesa criarMesa(@RequestBody Mesa mesa) {
-        return mesaService.criarMesa(mesa);
+        return ((Object) mesaService).criarMesa(mesa);
     }
 
     @GetMapping
     public List<Mesa> obterTodasMesas() {
-        return mesaService.obterTodasMesas();
+        return ((Object) mesaService).obterTodasMesas();
     }
 
     @GetMapping("/{id}")
     public Mesa obterMesaPorId(@PathVariable int id) {
-        return mesaService.obterMesaPorId(id);
+        return ((Object) mesaService).obterMesaPorId(id);
     }
 
     @PutMapping("/{id}")
     public Mesa atualizarMesa(@PathVariable int id, @RequestBody Mesa mesa) {
-        return mesaService.atualizarMesa(id, mesa);
+        return ((Object) mesaService).atualizarMesa(id, mesa);
     }
 
     @DeleteMapping("/{id}")
     public void removerMesa(@PathVariable int id) {
-        mesaService.removerMesa(id);
+        ((Object) mesaService).removerMesa(id);
     }
 }
