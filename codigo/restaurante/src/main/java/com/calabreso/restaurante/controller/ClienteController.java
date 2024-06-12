1a1eafb547818dfac4aaa479288d60982147a3b2
Pedro Lobato <pedrolfirbida@gmail.com>
2024-06-12 08:41:26 -0300

    Atualizado controller do cliente

--- codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/ClienteController.java
+++ codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/ClienteController.java
@@ -1,68 +1,32 @@
-package com.calabreso.restaurante;
+package com.calabreso.restaurante.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.calabreso.restaurante.entity.Cliente;
-import com.calabreso.restaurante.repository.ClienteRepository;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.List;
 
 @RestController
-@RequestMapping("/clientes")
 public class ClienteController {
 
-    @Autowired
-    private ClienteRepository clienteRepository; // Injeção do repositório de clientes
-
-    @PostMapping
-    public ResponseEntity<Cliente> criarCliente(@Valid @RequestBody Cliente cliente) {
-        if (cliente.getNomeCliente().length() <= 2) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        clienteRepository.save(cliente);
-        return ResponseEntity.created(URI.create("/clientes/" + cliente.getIdCliente())).body(cliente);
+    @PostMapping("/cliente/criar")
+    public Cliente criarCliente(@RequestParam("nome") String nome) {
+        return new Cliente(nome);
     }
 
-    @GetMapping
-    public List<Cliente> listarTodosClientes() {
-        return clienteRepository.findAll();
+    @GetMapping("/cliente/{id}")
+    public Cliente getCliente(@PathVariable("id") int id) {
+        return null;
     }
 
-    @GetMapping("/{idCliente}")
-    public ResponseEntity<Cliente> buscarClientePorId(@PathVariable int idCliente) {
-        Cliente cliente = clienteRepository.findById(idCliente).orElse(null);
-        if (cliente == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(cliente);
+    @DeleteMapping("/cliente/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable("id") int id) {
+      return ResponseEntity.noContent().build();
     }
-
-    @PutMapping("/{idCliente}")
-    public ResponseEntity<Cliente> atualizarCliente(@PathVariable int idCliente, @Valid @RequestBody Cliente clienteAtualizado) {
-        Cliente clienteExistente = clienteRepository.findById(idCliente).orElse(null);
-        if (clienteExistente == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        if (clienteAtualizado.getNomeCliente().length() <= 2) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        clienteExistente.setNomeCliente(clienteAtualizado.getNomeCliente());
-        clienteRepository.save(clienteExistente);
-        return ResponseEntity.ok(clienteExistente);
-    }
-
-    @DeleteMapping("/{idCliente}")
-    public ResponseEntity<Void> deletarCliente(@PathVariable int idCliente) {
-        clienteRepository.deleteById(idCliente);
-        return ResponseEntity.noContent().build();
-    }
+    
 }
--- codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/MesaController.java
+++ codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/MesaController.java
@@ -1,54 +1,54 @@
-package com.calabreso.restaurante.controller;
+// package com.calabreso.restaurante.controller;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+// import javax.persistence.EntityManagerFactory;
+// import javax.persistence.PersistenceUnit;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.calabreso.restaurante.entity.Mesa;
+// import com.calabreso.restaurante.entity.Mesa;
 
-@Controller
-@RequestMapping("/mesas")
-public class MesaController {
+// @Controller
+// @RequestMapping("/mesas")
+// public class MesaController {
 
-@PersistenceUnit
-private EntityManagerFactory entityManagerFactory;
+// @PersistenceUnit
+// private EntityManagerFactory entityManagerFactory;
 
-@PostMapping("/criar")
-@ResponseBody
-public Mesa criarMesa(@RequestBody Mesa mesa) {
-EntityManager em = entityManagerFactory.createEntityManager();
+// @PostMapping("/criar")
+// @ResponseBody
+// public Mesa criarMesa(@RequestBody Mesa mesa) {
+// EntityManager em = entityManagerFactory.createEntityManager();
 
-try {
-em.getTransaction().begin();
-em.persist(mesa);
-em.getTransaction().commit();
-} finally {
-if (em.isOpen()) {
-em.close();
-}
-}
+// try {
+// em.getTransaction().begin();
+// em.persist(mesa);
+// em.getTransaction().commit();
+// } finally {
+// if (em.isOpen()) {
+// em.close();
+// }
+// }
 
-return mesa;
-}
+// return mesa;
+// }
 
-@GetMapping("/buscar/{id}")
-@ResponseBody
-public Mesa getMesa(@PathVariable Integer id) {
-EntityManager em = entityManagerFactory.createEntityManager();
+// @GetMapping("/buscar/{id}")
+// @ResponseBody
+// public Mesa getMesa(@PathVariable Integer id) {
+// EntityManager em = entityManagerFactory.createEntityManager();
 
-try {
-Mesa mesa = em.find(Mesa.class, id);
-return mesa;
-} finally {
-if (em.isOpen()) {
-em.close();
-}
-}
-}
-}
+// try {
+// Mesa mesa = em.find(Mesa.class, id);
+// return mesa;
+// } finally {
+// if (em.isOpen()) {
+// em.close();
+// }
+// }
+// }
+// }
