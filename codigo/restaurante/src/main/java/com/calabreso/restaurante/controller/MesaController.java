5762c0a92b56fc7c7b30ef2e2afe53df9ce21883
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2024-06-13 14:15:40 -0300

    Update MesaController.java

--- codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/MesaController.java
+++ codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/MesaController.java
@@ -1,54 +1,81 @@
 package com.calabreso.restaurante.controller;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.calabreso.restaurante.entity.Mesa;
 
+/**
+ * Controlador responsável por gerenciar as operações relacionadas a entidades Mesa.
+ */
 @Controller
 @RequestMapping("/mesas")
 public class MesaController {
 
-@PersistenceUnit
-private EntityManagerFactory entityManagerFactory;
+    // Injeta a fábrica de EntityManager para gerenciar as entidades JPA
+    @PersistenceUnit
+    private EntityManagerFactory entityManagerFactory;
 
-@PostMapping("/criar")
-@ResponseBody
-public Mesa criarMesa(@RequestBody Mesa mesa) {
-EntityManager em = entityManagerFactory.createEntityManager();
+    /**
+     * Método para criar uma nova entidade Mesa.
+     * 
+     * @param mesa A entidade Mesa recebida no corpo da requisição.
+     * @return A entidade Mesa criada.
+     */
+    @PostMapping("/criar")
+    @ResponseBody
+    public Mesa criarMesa(@RequestBody Mesa mesa) {
+        // Cria um EntityManager a partir da fábrica
+        EntityManager em = entityManagerFactory.createEntityManager();
 
-try {
-em.getTransaction().begin();
-em.persist(mesa);
-em.getTransaction().commit();
-} finally {
-if (em.isOpen()) {
-em.close();
- }
- }
+        try {
+            // Inicia a transação
+            em.getTransaction().begin();
+            // Persiste a entidade Mesa no banco de dados
+            em.persist(mesa);
+            // Comita a transação
+            em.getTransaction().commit();
+        } finally {
+            // Fecha o EntityManager se estiver aberto
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
 
- return mesa;
-}
+        // Retorna a entidade Mesa criada
+        return mesa;
+    }
 
-@GetMapping("/buscar/{id}")
- @ResponseBody
- public Mesa getMesa(@PathVariable Integer id) {
- EntityManager em = entityManagerFactory.createEntityManager();
+    /**
+     * Método para buscar uma entidade Mesa pelo seu ID.
+     * 
+     * @param id O ID da entidade Mesa a ser buscada.
+     * @return A entidade Mesa encontrada, ou null se não for encontrada.
+     */
+    @GetMapping("/buscar/{id}")
+    @ResponseBody
+    public Mesa getMesa(@PathVariable Integer id) {
+        // Cria um EntityManager a partir da fábrica
+        EntityManager em = entityManagerFactory.createEntityManager();
 
-try {
-Mesa mesa = em.find(Mesa.class, id);
-return mesa;
-} finally {
-if (em.isOpen()) {
-em.close();
-}
-}
-}
+        try {
+            // Busca a entidade Mesa pelo ID
+            Mesa mesa = em.find(Mesa.class, id);
+            return mesa;
+        } finally {
+            // Fecha o EntityManager se estiver aberto
+            if (em.isOpen()) {
+                em.close();
+            }
+        }
+    }
 }
