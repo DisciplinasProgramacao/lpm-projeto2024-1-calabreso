22239be9b7bbc519818b8035c942786ff0e99e5b
Nelson de Campos Nolasco <nelsonnolasco@gmail.com>
2024-06-23 19:05:35 -0300

    Acertos controller requisicao

--- codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/RequisicaoController.java
+++ codigo/restaurante/src/main/java/com/calabreso/restaurante/controller/RequisicaoController.java
@@ -3,80 +3,35 @@ package com.calabreso.restaurante.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.calabreso.restaurante.entity.Cliente;
 import com.calabreso.restaurante.entity.Mesa;
+import com.calabreso.restaurante.entity.Requisicao;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RequestMapping(value = "/requisicao")
 @RestController
 public class RequisicaoController {
 
-    @PostMapping("/criar")
-    public Cliente criarCliente(@RequestParam("nome") String nome) {
-        return new Cliente(nome);
-    }
+    @PostMapping("/requisicoes/{idCliente}/{quantPessoas}")
+public @ResponseBody Requisicao novaRequisicao(@PathVariable Integer quantPessoas, @PathVariable Integer idCliente)
 
-    @GetMapping("/{id}")
-    public Cliente getCliente(@PathVariable("id") int id) {
-        return null;
-    }
+@GetMapping("/requisicoes/{id}")
+public @ResponseBody Requisicao buscarRequisicao(@PathVariable Integer id) {
 
-    @GetMapping("/mesa/{id}")
-    public Mesa getMesa(@PathVariable("id") int id) {
-        return null;
-    }
+@PutMapping("/requisicoes/pedir/{id}/{produto}")
+public @ResponseBody Requisicao addProduto(@PathVariable Integer id, @PathVariable Integer produto)
 
-    @PostMapping("/mesa/{id}/ocupar")
-    public void ocuparMesa(@PathVariable("id") int id) {
-    }
+@PutMapping("/requisicoes/alocar/{idReq}/{idMesa}")
+public @ResponseBody Requisicao alocarMesa(@PathVariable Integer idMesa, @PathVariable Integer idReq)
 
-    @PostMapping("/mesa/{id}/desocupar")
-    public void desocuparMesa(@PathVariable("id") int id) {
-    }
-
-    @GetMapping("/mesa/{id}/liberada/{quantPessoas}")
-    public boolean estahLiberada(@PathVariable("id") int id, @PathVariable("quantPessoas") int quantPessoas) {
-        return false;
-    }
-
-    @GetMapping("/mesa/{id}/descricao")
-    public void getDescricaoMesa(@PathVariable("id") int id) {
-    }
-
-    @GetMapping("/mesa/{id}/id")
-    public int getIdMesa(@PathVariable("id") int id) {
-        return id;
-    }
-
-    @GetMapping("/mesa/{id}/capacidade")
-    public int getCapacidadeMesa(@PathVariable("id") int id) {
-        return id;
-    }
-    
-    @GetMapping("/mesa/{id}/cliente/{idCliente}/descricao")
-    public String getDescricaoClienteMesa(@PathVariable("id") int id, @PathVariable("idCliente") int idCliente) {
-        return ""; 
-    }
-
-    @GetMapping("/mesa/{id}/cliente/{idCliente}/ocupada")
-    public boolean getOcupadaCliente(@PathVariable("id") int id, @PathVariable("idCliente") int idCliente) {
-        return false; 
-    }
-
-    @GetMapping("/mesa/{id}/cliente/{idCliente}/idMesa")
-    public int getIdMesaCliente(@PathVariable("id") int id, @PathVariable("idCliente") int idCliente) {
-        return 0; 
-    }
-
-    @GetMapping("/mesa/{id}/cliente/{idCliente}/id")
-    public int getIdCliente(@PathVariable("id") int id, @PathVariable("idCliente") int idCliente) {
-        return 0; 
-    }
+@PutMapping("/requisicoes/encerrar/{id}")
+public @ResponseBody Requisicao encerrarRequisicao(@PathVariable Integer id)
     
    
 }
